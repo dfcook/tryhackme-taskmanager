508a02408cb79dde7269230092a57e18157dd0d3
Daniel Cook <dfcook@hotmail.com>
2024-01-06 10:58:46 +0000

    feat:add nuxt ui

---
+++ packages/ui-nuxt/components/MaxWidthWrapper.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="$style.wrapper">
+    <slot />
+  </div>
+</template>
+
+<style scss module>
+.wrapper {
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  max-width: 1280px;
+  padding-left: 0.625rem;
+  padding-right: 0.625rem;
+  @media (min-width: 768px) {
+    padding-left: 5rem;
+    padding-right: 5rem;
+  }
+}
+</style>
